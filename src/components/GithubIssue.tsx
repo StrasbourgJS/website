f7bfe9c091d941edb2b54d5e4ed55a02311e1331
Michel EDIGHOFFER <edimitchel@gmail.com>
2023-03-08 17:01:09 +0100

    feat: issue content clean up

--- src/components/GithubIssue.tsx
+++ src/components/GithubIssue.tsx
@@ -1,19 +1,31 @@
-import { FiChevronDown } from "react-icons/fi";
+import { useState } from "react";
+import {
+  FiArrowUpRight,
+  FiChevronDown,
+  FiChevronUp,
+  FiCodepen,
+} from "react-icons/fi";
 
 export const GithubIssue = ({ issue }: any) => {
-  return (
-    <details>
-      <summary className="list-none">
-        <div className="flex flex-row gap-4">
-          <FiChevronDown className={"h-6 w-6"} aria-hidden />
+  const [isOpen, setIsOpen] = useState(false);
 
-          <div>
-            <h3 className="font-bold text-lg">{issue.title}</h3>
+  return (
+    <details onToggle={() => setIsOpen((state) => !state)}>
+      <summary className="list-none cursor-pointer">
+        <div className="flex flex-row items-center gap-4">
+          {isOpen ? (
+            <FiChevronUp className={"h-6 w-6"} aria-hidden />
+          ) : (
+            <FiChevronDown className={"h-6 w-6"} aria-hidden />
+          )}
+
+          <div className="flex-grow">
+            <h3 className="text-lg font-bold">{issue.title}</h3>
             <p className="text-gray-600">
               Par{" "}
               <a
                 href={issue.user.html_url}
-                className="text black font-semibold underline"
+                className="font-semibold underline text black"
                 target={"_blank"}
                 rel="noopener noreferrer"
               >
@@ -21,11 +33,14 @@ export const GithubIssue = ({ issue }: any) => {
               </a>
             </p>
           </div>
+          <a href={issue.html_url} target="_blank" rel="nofollow">
+            <FiArrowUpRight className={"h-9 w-9 p-2 hover:text-slate-400"} />
+          </a>
         </div>
       </summary>
       <div
         dangerouslySetInnerHTML={{ __html: issue.body }}
-        className="pt-10 prose prose-sm prose-slate leading-relaxed"
+        className="pt-4 pl-10 leading-relaxed prose-sm prose prose-slate"
       />
     </details>
   );
--- src/services/getMileStoneByEventId.ts
+++ src/services/getMileStoneByEventId.ts
@@ -1,12 +1,16 @@
 import { marked } from "marked";
 
+const headers = process.env.GITHUB_TOKEN
+  ? {
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      },
+    }
+  : undefined;
+
 export const getMileStoneByEventId = async (eventId: string) => {
   const endpoint = `https://api.github.com/repos/StrasbourgJS/talks/milestones`;
-  const data = await fetch(endpoint, {
-    headers: {
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-    },
-  }).then((res) => res.json());
+  const data = await fetch(endpoint, headers).then((res) => res.json());
 
   const milestone = data.find((d: any) =>
     d.description.includes(`/events/${eventId}`)
@@ -14,19 +18,25 @@ export const getMileStoneByEventId = async (eventId: string) => {
 
   const issues = await fetch(
     `https://api.github.com/repos/StrasbourgJs/talks/issues?milestone=${milestone.number}&state=all`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-    }
+    headers
   ).then((res) => res.json());
 
   const renderer = new marked.Renderer();
   renderer.heading = (text, level) => `<h${level}>${text}</h${level}>`;
   marked.setOptions({ renderer });
 
-  return (issues || []).map((issue: any) => ({
-    ...issue,
-    body: marked(issue.body),
-  }));
+  return (issues || []).map(transformIssue);
 };
+
+function transformIssue(issue: any) {
+  const rawBody = (issue.body as string).replace(
+    /.*### Contenu(.*)### Durée.*/gms,
+    "$1"
+  );
+  const body = marked(rawBody);
+
+  return {
+    ...issue,
+    body,
+  };
+}
