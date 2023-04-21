487c659b8985aeb96d13ae2cf4f4639d9718dfe5
mfrachet <undefined>
2023-04-21 06:37:17 +0200

    fox

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,10 @@ export interface HeroProps {
 export const Hero = ({ children }: HeroProps) => {
   return (
     <div
-      className="py-20 flex items-center"
       style={{
         background: "#5012FF",
       }}
+      className="py-20 flex items-center"
     >
       <div className="max-w-5xl mx-auto px-4 w-full">{children}</div>
     </div>
